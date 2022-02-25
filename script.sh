1b9624b23d315e4cd4770afb950c9608270b1707
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-02-25 20:25:54 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+wget -O fahclient.deb https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/latest.deb
+sudo dpkg -i --force-depends fahclient.deb
+
+
+
+
+
 apt-get -y update
 apt-get install screen
 sudo wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
