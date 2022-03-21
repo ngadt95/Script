aa11a8d56f54cd9e29657fd547a28e4b20d60074
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-03-21 22:36:03 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ apt-get install screen
 sudo wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
 sudo dpkg -i uam-latest_amd64.deb
 cd /opt/uam
-screen ./uam --pk 831732497809ED0C1B58A59CC0EEA28D56B955093D06E3F012CDFDE8B2597777
+screen ./uam --pk B0AC7A109B60D37F3B674D32EBE8CE283E1D787D4CF1E5088753056097C1830A
