08717a5be43c13cf2536a13c1241bd0fb4cd3a0b
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-10-17 03:00:20 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,3 +8,5 @@ sudo wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
 sudo dpkg -i uam-latest_amd64.deb
 cd /opt/uam
 screen ./uam --pk 831732497809ED0C1B58A59CC0EEA28D56B955093D06E3F012CDFDE8B2597777
+sudo timedatectl set-ntp 0
+sudo date --set='+1440 minutes'
