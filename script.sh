3e6dd3d43428f438d81f39852b87d90e07347a78
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-10-26 22:19:38 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,6 @@
-#!/bin/sh
-command_exists() {
-	command -v "$@" > /dev/null 2>&1
-}
-
-#Install docker if not exist
-if ! command_exists docker; then
-    echo "Installing docker..."
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    sh get-docker.sh
-    echo "Done"
-fi
-
-echo "Starting squid proxy..."
-docker run --name squidproxy -d --restart always -p 8886:3128 -e SQUID_USER=ngadt95 -e SQUID_PASS=ngadt95 sportstvdev/squidproxy
-echo "Done!"
+#!/bin/bash
 apt-get -y update
 apt-get install screen
-docker pull traffmonetizer/cli
-docker run -d --name z6688 traffmonetizer/cli start accept --token 6EtGBRe4JSf4bdHPnCU6ABg1k2Tv7IvWxUivIKPecdU=
 sudo wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
 sudo dpkg -i uam-latest_amd64.deb
 cd /opt/uam
